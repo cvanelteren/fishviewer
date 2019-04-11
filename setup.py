5474086ddb1dfc0b09f4abaa936a71023fed61ec
cvanelteren <caspervanelteren@gmail.com>
2019-04-12 00:35:58 +0200

    updated but not complete setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 # from distutils.core import setup, Extension
-from setuptools import setup
-from setuptools.extension import Extension
+from setuptools import setup, find_packages
 # MODULE INFORMATION
 __module__              = "fishviewer"
 __author__              = "Casper van Elteren"
@@ -14,14 +13,12 @@ with open('requirements.txt', 'r') as f:
     requirements =  f.readlines()
 requirements = [i.strip() for i in requirements]
 # compile
-with open('requirements.txt', 'r') as install_reqs:
-    setup(\
-        name            = __name__,\
-        author          = __author__,\
-        author_email    = __email__,\
-        python_requires = __python__requires__,\
-        zip_safe        = False,\
-        install_requires= requirements,\
-
-    # gdb_debug =True,
-    )
+print(requirements)
+setup(\
+    version         = "1.0",\
+    name            = __name__,\
+    author          = __author__,\
+    author_email    = __email__,\
+    python_requires = __python__requires__,\
+    packages        = find_packages(),\
+)
